2f27596219ff46b0ef780af532387fafbd86a281
Muhammad Akash <210akash@gmail.com>
2026-03-13 10:22:53 +0500

    Changes

---
+++ ERP.Mediator/Mediator/PurchaseOrder/Handler/GetPendingDemandQuery.cs
@@ -0,0 +1,16 @@
+﻿using ERP.BusinessModels.ResponseVM;
+using MediatR;
+using System.Collections.Generic;
+
+namespace ERP.Mediator.Mediator.PurchaseOrder.Query
+{
+    public class GetPendingDemandQuery : IRequest<List<GetDropDown>>
+    {
+        //public GetPendingDemandQuery(List<long> PurchaseDemandId)
+        //{
+        //    this.PurchaseDemandId = PurchaseDemandId;
+        //}
+
+        public List<long> PurchaseDemandId { get; set; }
+    }
+}

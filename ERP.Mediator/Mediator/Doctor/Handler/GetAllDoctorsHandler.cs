608c8742cdb976454fd208c581c927a5ec01669a
210akash <210akash@gmail.com>
2026-04-01 00:29:17 +0500

    Change

--- ERP.Mediator/Mediator/Doctor/Handler/GetAllDoctorsHandler.cs
+++ ERP.Mediator/Mediator/Doctor/Handler/GetAllDoctorsHandler.cs
@@ -28,32 +28,46 @@ namespace ERP.Mediator.Mediator.Doctor.Handler
 
         public async Task<Tuple<List<GetAllUsers>, long>> Handle(GetAllDoctorsQuery request, CancellationToken cancellationToken)
         {
-            var doctorrole = await unitOfWork.Repository<Entities.Models.AspNetRoles>().GetFirstAsNoTrackingAsync(x => x.Name.ToLower() == "doctor");
-            Expression<Func<Entities.Models.AspNetUsers, bool>> predicate = x => x.IsActive
-                && (request.DepartmentId == 0 || request.DepartmentId == null || x.DepartmentId == request.DepartmentId)
-                && x.UserProject.Any(y => y.ProjectId == sessionProvider.Session.SelectedWarehouseId)
-                && x.AspNetUserRoles.Any(y => y.RoleId == doctorrole.Id)
-                && (string.IsNullOrEmpty(request.Name) || x.FirstName.ToLower().Contains(request.Name.ToLower()) || x.LastName.ToLower().Contains(request.Name.ToLower()))
-                && (request.EmployeeDesignationId == 0 || request.EmployeeDesignationId == null || x.EmployeeDesignationId == request.EmployeeDesignationId);
+            var doctorrole = await unitOfWork.Repository<Entities.Models.AspNetRoles>()
+                .GetFirstAsNoTrackingAsync(x => x.Name == "Doctor");
+
+            if (doctorrole == null)
+                return new Tuple<List<GetAllUsers>, long>(new List<GetAllUsers>(), 0);
+
+            Expression<Func<Entities.Models.AspNetUsers, bool>> predicate = x =>
+                x.IsActive &&
+                (request.DepartmentId == null || request.DepartmentId == 0 || x.DepartmentId == request.DepartmentId) &&
+                (request.EmployeeDesignationId == null || request.EmployeeDesignationId == 0 || x.EmployeeDesignationId == request.EmployeeDesignationId) &&
+                x.UserProject.Any(y => y.ProjectId == sessionProvider.Session.SelectedWarehouseId) &&
+                x.AspNetUserRoles.Any(y => y.RoleId == doctorrole.Id) &&
+                (string.IsNullOrEmpty(request.Name) ||
+                    x.FirstName.Contains(request.Name) ||
+                    x.LastName.Contains(request.Name)
+                );
 
             Expression<Func<Entities.Models.AspNetUsers, object>>[] includes = {
-                x => x.AspNetUserRoles,
-                x => x.Department,
-                x => x.UserProject,
-                x => x.Attachments,
-                x => x.EmployeeDesignation,
-            };
+        x => x.AspNetUserRoles,
+        x => x.Department,
+        x => x.UserProject,
+        x => x.Department.Company,
+        x => x.Store,
+        x => x.Attachments,
+        x => x.EmployeeDesignation,
+    };
 
             List<string> thenIncludes = new()
-            {
-                "AspNetUserRoles.Role"
-            };
+    {
+        "AspNetUserRoles.Role"
+    };
 
             Expression<Func<Entities.Models.AspNetUsers, object>> OrderBy = null;
             Expression<Func<Entities.Models.AspNetUsers, object>> OrderByDesc = x => x.ModifiedDate ?? x.CreatedDate;
 
-            var entity = unitOfWork.Repository<Entities.Models.AspNetUsers>().GetPagingWhereAsNoTrackingAsync(predicate, request.PagingData, OrderBy, OrderByDesc, thenIncludes, includes);
+            var entity = unitOfWork.Repository<Entities.Models.AspNetUsers>()
+                .GetPagingWhereAsNoTrackingAsync(predicate, request.PagingData, OrderBy, OrderByDesc, thenIncludes, includes);
+
             var users = mapper.Map<IEnumerable<GetAllUsers>>(entity.Item1).ToList();
+
             return new Tuple<List<GetAllUsers>, long>(users, entity.Item2);
         }
     }
